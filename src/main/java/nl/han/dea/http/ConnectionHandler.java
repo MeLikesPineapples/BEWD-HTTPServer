10e0b57a5aa9d70261b73bcaed0a276dea05a096
Wiebe Rinsma <wiebe.rinsma@han.nl>
2024-09-17 08:30:44 +0200

    Use unused methods/class, close streams, fix fotmatting

--- src/main/java/nl/han/dea/http/ConnectionHandler.java
+++ src/main/java/nl/han/dea/http/ConnectionHandler.java
@@ -9,8 +9,8 @@ import java.time.OffsetDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Objects;
 
-public class ConnectionHandler {
-
+public class ConnectionHandler
+{
     private static final String SERVER_NAME = "Simple DEA Webserver";
     private static final String HTTP_STATUS_200 = "200 OK";
     private static final String HTTP_STATUS_404 = "404 NOT FOUND";
@@ -26,12 +26,13 @@ public class ConnectionHandler {
 
     private final Socket socket;
 
-    public ConnectionHandler(final Socket socket) {
+    public ConnectionHandler(final Socket socket)
+    {
         this.socket = socket;
     }
 
-    public void accept() throws IOException {
-
+    public void accept() throws IOException
+    {
         // We create a BufferedReader that reads the HTTP-Request.
         var inputStreamReader = new BufferedReader(
                 new InputStreamReader(socket.getInputStream(), StandardCharsets.US_ASCII));
@@ -43,107 +44,120 @@ public class ConnectionHandler {
         parseHttpRequest(inputStreamReader, outputStreamWriter);
 
         writeResponseMessage(outputStreamWriter);
+
+        inputStreamReader.close();
+        outputStreamWriter.close();
     }
 
-    private void parseHttpRequest(final BufferedReader inputStreamReader, final BufferedWriter outputStreamWriter) throws IOException {
+    private void parseHttpRequest(final BufferedReader inputStreamReader, final BufferedWriter outputStreamWriter) throws IOException
+    {
         var startLine = true;
         String requestLine;
 
-        while ((requestLine = inputStreamReader.readLine()) != null) {
-            if (startLine) {
+        while ((requestLine = inputStreamReader.readLine()) != null)
+        {
+            if (startLine)
+            {
                 System.out.println("-> Received the following HTTP-request:");
             }
             System.out.println(requestLine);
-            if (startLine) {
+            if (startLine)
+            {
                 startLine = false;
                 processStartLine(outputStreamWriter, requestLine);
-            } else {
-                if (lineMarksEndOfRequest(requestLine)) {
+            } else
+            {
+                if (lineMarksEndOfRequest(requestLine))
+                {
                     return;
                 }
             }
         }
     }
 
-    private void processStartLine(final BufferedWriter outputStreamWriter, final String requestLine) throws IOException {
+    private void processStartLine(final BufferedWriter outputStreamWriter, final String requestLine) throws IOException
+    {
         var startLineTokens = requestLine.split(" ");
 
         checkForUnsupportedMethods(outputStreamWriter, startLineTokens);
         checkForUnsupportedHTTPVersions(outputStreamWriter, startLineTokens);
 
-
-        if (!"/index.html".equals(startLineTokens[1])) {
+        if (!"/index.html".equals(startLineTokens[1]))
+        {
             outputStreamWriter.write(generateHeader(HTTP_STATUS_404, null));
             outputStreamWriter.newLine();
             outputStreamWriter.flush();
         }
     }
 
-    private void checkForUnsupportedMethods(final BufferedWriter outputStreamWriter, String[] startLineTokens) throws IOException {
-        if (!"GET".equals(startLineTokens[0])) {
+    private void checkForUnsupportedMethods(final BufferedWriter outputStreamWriter, String[] startLineTokens) throws IOException
+    {
+        if (!"GET".equals(startLineTokens[0]))
+        {
             outputStreamWriter.write(generateHeader(HTTP_STATUS_501, null));
             outputStreamWriter.newLine();
             outputStreamWriter.flush();
         }
     }
 
-    private void checkForUnsupportedHTTPVersions(final BufferedWriter outputStreamWriter, String[] startLineTokens) throws IOException {
-        if (!"HTTP/1.1".equals(startLineTokens[2])) {
+    private void checkForUnsupportedHTTPVersions(final BufferedWriter outputStreamWriter, String[] startLineTokens) throws IOException
+    {
+        if (!"HTTP/1.1".equals(startLineTokens[2]))
+        {
             outputStreamWriter.write(generateHeader(HTTP_STATUS_505, null));
             outputStreamWriter.newLine();
             outputStreamWriter.flush();
         }
     }
 
-    private void writeResponseMessage(final BufferedWriter outputStreamWriter) {
-
-        try {
+    private void writeResponseMessage(final BufferedWriter outputStreamWriter)
+    {
+        try
+        {
             outputStreamWriter.write(generateHeader(HTTP_STATUS_200, INDEX_HTML_PAGE));
             outputStreamWriter.newLine();
             outputStreamWriter.write(readFile(INDEX_HTML_PAGE));
             outputStreamWriter.newLine();
             outputStreamWriter.flush();
-        } catch (IOException e) {
+        } catch (IOException e)
+        {
             throw new RuntimeException(e);
         }
     }
 
-    private String readFile(final String filename) {
-        try {
-            return new String(Files.readAllBytes(getPath(filename)));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+    private String readFile(final String filename)
+    {
+        return new HtmlPageReader().readFile(filename);
     }
 
-    private String generateHeader(final String status, final String filename) {
-        var header = HEADER_TEMPLATE
+    private String generateHeader(final String status, final String filename)
+    {
+        var headers = HEADER_TEMPLATE
                 .replace("{{DATE}}",
                         OffsetDateTime.now().format(DateTimeFormatter.RFC_1123_DATE_TIME))
                 .replace("{{HTTP_STATUS}}", status);
 
-
-        header = header.replace("{{CONTENT_LENGTH}}", Integer.toString(90));
+        headers = setContentLength(headers, filename);
 
         System.out.println("-> Responded with the following HTTP-headers:");
-        System.out.println(header);
-        return header;
+        System.out.println(headers);
+        return headers;
     }
 
-    private String setContentLength(final String header, final String filename) {
-        var contentLength = Long.toString(0);
-        if (filename != null) {
-            contentLength = Long.toString(getPath(filename).toFile().length());
-        }
-        return header.replace("{{CONTENT_LENGTH}}", contentLength);
+    private String setContentLength(final String headers, final String filename)
+    {
+        var contentLength = filename != null ? getPath(filename).toFile().length() : 0;
+        return headers.replace("{{CONTENT_LENGTH}}", Long.toString(contentLength));
     }
 
-    private Path getPath(final String filename) {
+    private Path getPath(final String filename)
+    {
         var classLoader = getClass().getClassLoader();
         return new File(Objects.requireNonNull(classLoader.getResource(filename)).getFile()).toPath();
     }
 
-    private boolean lineMarksEndOfRequest(String line) {
+    private boolean lineMarksEndOfRequest(String line)
+    {
         return line.isEmpty();
     }
 }
