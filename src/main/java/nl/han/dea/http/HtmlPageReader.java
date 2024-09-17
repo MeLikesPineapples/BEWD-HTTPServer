e79529660e220b1ddfe69c770f5641665826b1a2
Wiebe Rinsma <wiebe.rinsma@han.nl>
2024-09-17 08:30:44 +0200

    Use unused methods/class, close streams, fix fotmatting

--- src/main/java/nl/han/dea/http/HtmlPageReader.java
+++ src/main/java/nl/han/dea/http/HtmlPageReader.java
@@ -5,17 +5,18 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.util.Objects;
 
-public class HtmlPageReader {
-
-    public String readFile(String filename) {
-        var fullFileName = "pages/".concat(filename);
-        try {
+public class HtmlPageReader
+{
+    public String readFile(String filename)
+    {
+        try
+        {
             ClassLoader classLoader = getClass().getClassLoader();
-
-            var file = new File(Objects.requireNonNull(classLoader.getResource(fullFileName)).getFile()).toPath();
+            var file = new File(Objects.requireNonNull(classLoader.getResource(filename)).getFile()).toPath();
 
             return new String(Files.readAllBytes(file));
-        } catch (IOException e) {
+        } catch (IOException e)
+        {
             throw new RuntimeException(e);
         }
     }
